0e35ba5460b28e3dc05258378d79bd310734e1a0
Johannes Letsoalo <letsoalojohannes6@gmail.com>
2023-10-24 18:22:56 -0700

    file name xhange

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    indx = 0
+    while indx < x:
+        try:
+            print("{}".format(my_list[indx]), end="")
+        except IndexError:
+            break
+        indx += 1
+    print("")
+    return indx
